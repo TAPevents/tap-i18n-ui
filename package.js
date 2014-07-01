1fd02bae6ee9331ace7bdab7c81cb7c7d049d337
Chris Hitchcott <hitchcott@gmail.com>
2014-07-01 12:03:27 +0800

    Depend on tap-i18n

--- package.js
+++ package.js
@@ -5,8 +5,6 @@ Package.describe({
 Package.on_use(function (api) {
   api.use(['coffeescript','underscore','templating','amplify','tap-i18n'], 'client');
 
-  api.imply('tap-i18n', ['client', 'server']);
-
   api.add_files([
     'i18n_ui.html',
     'i18n_ui.coffee',
